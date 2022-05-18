9149bcb70330a402143141ea75d477b8113ffb9f
mariuszdl <maniek.14.12.2@gmail.com>
2022-05-18 09:44:34 +0200

    Dodanie pomiaru czasu wykonania metody main klasy TestBinCopy

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab5/console/TestBinCopy.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab5/console/TestBinCopy.java
@@ -11,6 +11,8 @@ public class TestBinCopy {
         FileInputStream in = null;
         FileOutputStream out = null;
 
+        long startTime = System.nanoTime();
+
         try {
             in = new FileInputStream("sample_1920x1280.tiff");
             out = new FileOutputStream("img_copy.tiff");
@@ -30,5 +32,11 @@ public class TestBinCopy {
                 out.close();
             }
         }
+
+        long endTime = System.nanoTime();
+        long timeElapsed = endTime - startTime;
+
+        System.out.println("Czas wykonania w nanosekundach: " + timeElapsed);
+        System.out.println("Czas wykonania w milisekundach: " + timeElapsed / 1000000);
     }
 }
