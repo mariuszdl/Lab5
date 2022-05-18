b61255fb66cdf927bf108fb2f63a538688b77ecd
mariuszdl <maniek.14.12.2@gmail.com>
2022-05-18 09:34:04 +0200

    Wprowadzanie kodu do klasy TestBinCopy

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab5/console/TestBinCopy.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab5/console/TestBinCopy.java
@@ -1,4 +1,34 @@
 package pl.lublin.wsei.java.cwiczenia.lab5.console;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class TestBinCopy {
+
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+            in = new FileInputStream("sample_1920x1280.tiff");
+            out = new FileOutputStream("img_copy.tiff");
+            int c;
+
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
 }
