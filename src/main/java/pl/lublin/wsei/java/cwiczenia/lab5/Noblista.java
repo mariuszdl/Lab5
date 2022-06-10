5d063c4bbaa1c7a73f00068b3293ee8e41a7e898
Mariusz Dłubak <maniek.14.12.2@gmail.com>
2022-06-10 22:05:27 +0200

    Utworzenie klasy Noblista przechowującej dane z wybranego wiersza z pliku CSV

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab5/Noblista.java
@@ -0,0 +1,24 @@
+package pl.lublin.wsei.java.cwiczenia.lab5;
+
+public class Noblista {
+    public String imie;
+    public String nazwisko;
+    public String rok;
+    public String kategoria;
+    public String uzasadnienie;
+    public String kraj;
+    public String dataRow;
+
+    public Noblista(String tekst) {
+        String[] items = tekst.split(",(?=\\S)");
+        dataRow = items[0];
+        imie = items[1];
+        nazwisko = items[2];
+        rok = items[12];
+        kategoria = items[13];
+        uzasadnienie = items[16];
+        if(items.length > 19) {
+            kraj = items[19];
+        }
+    }
+}
