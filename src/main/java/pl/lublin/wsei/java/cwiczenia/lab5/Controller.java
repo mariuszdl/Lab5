ac41cc1be6e2eadbfe51c8a6726b4b086d80ccbe
Mariusz Dłubak <maniek.14.12.2@gmail.com>
2022-06-10 22:06:52 +0200

    Przypisanie danych do ListView oraz obsługa zdarzenia wyboru elementu na liście

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab5/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab5/Controller.java
@@ -1,10 +1,59 @@
 package pl.lublin.wsei.java.cwiczenia.lab5;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class Controller {
+    public ListView lvNoblisci;
+    public TextField tfRok;
+    public TextField tfDziedzina;
+    public TextField tfKraj;
+    public TextField tfMotywacja;
+    private Noblista aNoblista;
+
+    ObservableList<String> tytuly = FXCollections.observableArrayList();
+    NoblisciList noblisciList;
+
+    @FXML
+    public void initialize() throws IOException {
+        noblisciList = new NoblisciList();
+
+        for(Noblista nb: noblisciList.noblisci) {
+            tytuly.add(nb.dataRow + " " + nb.imie + " " + nb.nazwisko);
+        }
+        lvNoblisci.setItems(tytuly);
+
+        lvNoblisci.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number old_val, Number new_val) {
+                int index = new_val.intValue();
+                if(index != -1) {
+                    tfRok.setText("");
+                    tfDziedzina.setText("");
+                    tfKraj.setText("");
+                    tfMotywacja.setText("");
+                    aNoblista = noblisciList.noblisci.get(index);
+                    tfRok.setText(aNoblista.rok);
+                    tfDziedzina.setText(aNoblista.kategoria);
+                    tfKraj.setText(aNoblista.kraj);
+                    tfMotywacja.setText(aNoblista.uzasadnienie);
+                } else {
+                    tfRok.setText("");
+                    tfDziedzina.setText("");
+                    tfKraj.setText("");
+                    tfMotywacja.setText("");
+                }
+            }
+        });
+    }
 
     public void btnExport(ActionEvent actionEvent) {
 
